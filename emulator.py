0e0d079f1f9130dccfe7039177e57ff3a6104671
Piotr <pb394091@students.mimuw.edu.pl>
2024-01-07 01:05:22 +0100

    Load program to memory

--- emulator.py
+++ emulator.py
@@ -1,7 +1,7 @@
-import time
 import ui
 import clock
 import utils
+import config
 
 class Emulator:
     def __init__(self):
@@ -9,16 +9,24 @@ class Emulator:
         self.executed_cycles = 0
         self.renderer = ui.UiRenderer(self)
         self.clock = clock.Clock(self, self.renderer)
+        self.memory = [0] * config.MEMORY_SIZE_KB * 1024
+        self.program_counter = config.PROGRAM_START_LOCATION
 
     def run(self):
         self.t_start_ms = utils.get_current_time_ms()
         self.clock.start()
 
+    def load_program_to_memory(self, code: [int]):
+        for index, byte in enumerate(code):
+            self.memory[config.PROGRAM_START_LOCATION + index] = byte
+
     def execute_cycle(self):
         self.executed_cycles += 1
 
     def get_running_time_ms(self) -> int:
-        return self.get_current_time_ms() - self.t_start_ms
+        return utils.get_current_time_ms() - self.t_start_ms
 
-    def get_current_time_ms(self) -> int:
-        return time.time_ns() / 1_000_000
+    def get_memory_window(self, size: int) -> [int]:
+        begin = self.program_counter
+        end = begin + size
+        return self.memory[begin:end]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import emulator as emul
 
 PROGRAM_CODE = [
-    '6007', # Sets V0 to 7
+    '600A', # Sets V0 to 7
     'A859', # Sets I to 2137
     '00E0', # Clear screen
     'D3B5', # Draw 5 bytes at (V3, V11)
@@ -12,49 +12,15 @@ PROGRAM_CODE = [
 def main():
     print("Starting CHIP-8 Emulator")
     emulator = emul.Emulator()
+    emulator.load_program_to_memory(convert_code_human_to_hex(PROGRAM_CODE))
     emulator.run()
 
-def get_code_human_readable(file_path: str) -> [str]:
-    code: [str] = []
-    with open(file_path, "rb") as file:
-        content: bytes = file.read()
-        for index in range(0, len(content), 2):
-            first_byte = get_human_readable_byte(content[index])
-            second_byte = get_human_readable_byte(content[index + 1])
-            code.append(first_byte + second_byte)
-    return code
-
-def get_human_readable_byte(byte: any) -> str:
-    if int(byte) < 16:
-        return f'0{hex(byte)[-1]}'
-    return (hex(byte)[-2:]).upper()
-
-def interpret_code(code: [str]):
-    for index, instr in enumerate(code):
-        print(f'{str(index).zfill(3)} - ', end='')
-        interpret_instruction(instr)
-
-def interpret_instruction(instr: str):
-    print(f'{instr} - ', end='')
-    if instr[0] == '6':
-        register = int(instr[1], 16)
-        value = int(instr[2:], 16)
-        print(f'Set register V{register} to {value}')
-    elif instr[0] == 'A':
-        value = int(instr[1:], 16)
-        print(f'Set register I to {value}')
-    elif instr == '00E0':
-        print('Clear the screen')
-    elif instr[0] == 'D':
-        x_register = int(instr[1], 16)
-        y_register = int(instr[2], 16)
-        n = int(instr[3 ], 16)
-        print(f'Draw {n} bytes at (V{x_register}, V{y_register})')
-    elif instr[0] == '1':
-        value = int(instr[1:], 16)
-        print(f'Jump to address {value}')
-    else:
-        print(f'Instruction {instr} not known')
+def convert_code_human_to_hex(code: [str]) -> [int]:
+    res = []
+    for instr in code:
+        res.append(int(instr[0:2], 16))
+        res.append(int(instr[2:4], 16))
+    return res
 
 if __name__ == "__main__":
     main()
