5717496064f6510f9a0b490cbab7f79e611d2a9f
Piotr <pb394091@students.mimuw.edu.pl>
2024-01-07 01:38:24 +0100

    Two of register instructions execution

--- emulator.py
+++ emulator.py
@@ -11,6 +11,8 @@ class Emulator:
         self.clock = clock.Clock(self, self.renderer)
         self.memory = [0] * config.MEMORY_SIZE_KB * 1024
         self.program_counter = config.PROGRAM_START_LOCATION
+        self.v_registers = [0] * config.V_REGISTERS_NUMBER
+        self.index_register = 0
 
     def run(self):
         self.t_start_ms = utils.get_current_time_ms()
@@ -22,6 +24,52 @@ class Emulator:
 
     def execute_cycle(self):
         self.executed_cycles += 1
+        instr = self.fetch_instruction_human_readable()
+        self.execute_instruction(instr)
+        self.move_program_counter()
+
+    def execute_instruction(self, instr: int):
+        if instr[0] == '6':
+            register = int(instr[1], 16)
+            value = int(instr[2:], 16)
+            self.set_v_register(register, value)
+            self.renderer.put_message(f'Set register V{register} to {value}')
+        elif instr[0] == 'A':
+            value = int(instr[1:], 16)
+            self.set_index_register(value)
+            self.renderer.put_message(f'Set register I to {value}')
+        elif instr == '00E0':
+            self.renderer.put_message('Clear the screen')
+        elif instr[0] == 'D':
+            x_register = int(instr[1], 16)
+            y_register = int(instr[2], 16)
+            n = int(instr[3 ], 16)
+            self.renderer.put_message(f'Draw {n} bytes at (V{x_register}, V{y_register})')
+        elif instr[0] == '1':
+            value = int(instr[1:], 16)
+            self.renderer.put_message(f'Jump to address {value}')
+        else:
+            self.renderer.put_message(f'Instruction {instr} not known')
+
+    def move_program_counter(self):
+        self.program_counter += 2
+
+    def fetch_instruction_human_readable(self) -> str:
+        first_byte = self.memory[self.program_counter]
+        second_byte = self.memory[self.program_counter + 1]
+        return utils.byte_to_human(first_byte) + utils.byte_to_human(second_byte)
+
+    def set_v_register(self, register: int, value: int):
+        self.v_registers[register] = value
+
+    def get_v_register(self, register: int) -> int:
+        return self.v_registers[register]
+
+    def set_index_register(self, value: int):
+        self.index_register = value
+
+    def get_index_register(self) -> int:
+        return self.index_register
 
     def get_running_time_ms(self) -> int:
         return utils.get_current_time_ms() - self.t_start_ms
--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ class UiRenderer:
     def __init__(self, emulator):
         self.executed_ui_updates = 0
         self.emulator = emulator
+        self.messages = []
 
     def update_ui(self):
         self.clear_console()
@@ -20,15 +21,40 @@ class UiRenderer:
         print(f'Running: {running_time / 1000:.3f}')
         print(f'Cycle: {executed_cycles + 1}')
         print('')
+        self.render_index_register()
+        print('')
+        self.render_registers()
+        print('')
         self.render_memory()
+        print('')
+        self.render_messages()
 
     def render_memory(self):
         memory = self.emulator.get_memory_window(config.UI_MEMORY_WINDOW_SIZE)
         current_location = self.emulator.program_counter
         print('Memory:')
         for index, byte in enumerate(memory):
-            print(f'\t{utils.leading_zeros(current_location + index, 4)}: {utils.byte_to_human(byte)}')
+            output = f'\t{utils.leading_zeros(current_location + index, 4)}: {utils.byte_to_human(byte)}'
+            if index == 0:
+                print(f'{output} <=')
+            else:
+                print(f'{output}')
 
+    def render_messages(self):
+        print('Messages:')
+        for message in self.messages[-config.UI_MESSAGES_WINDOW_SIZE:]:
+            print(f'\t{message}')
+
+    def render_registers(self):
+        print('Registers:')
+        for index, register in enumerate(self.emulator.v_registers):
+            print(f'\tV{index}: {register}')
+
+    def render_index_register(self):
+        print(f'Index register: {self.emulator.index_register}')
+
+    def put_message(self, message: str):
+        self.messages.append(message)
 
     def clear_console(self):
         os.system('clear')
